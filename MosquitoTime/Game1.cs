ab9e76710ec9d836a377a28b4a7d2717a28d4d51
EvanHolmes <squippysquerp@gmail.com>
2023-03-02 14:43:13 -0700

    Barely dragged along, He put the rest of it on moodle

--- MosquitoTime/Game1.cs
+++ MosquitoTime/Game1.cs
@@ -9,6 +9,13 @@ namespace MosquitoTime
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Texture2D playerCannonTexture;
+
+        GameObject testObject;
+        Transform testTransform;
+        Sprite testSprite;
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -17,18 +24,22 @@ namespace MosquitoTime
         }
 
         protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
+        {// TODO: Add your initialization logic here
             base.Initialize();
+
+            testTransform = new Transform();
+            testSprite = new Sprite();
+            testObject = new GameObject(testSprite, testTransform);
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            playerCannonTexture = Content.Load<Texture2D>("");
             // TODO: use this.Content to load your game content here
         }
+
+
 
         protected override void Update(GameTime gameTime)
         {
@@ -38,12 +49,18 @@ namespace MosquitoTime
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+
+            testObject.Update(gameTime);
         }
+
+
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            _spriteBatch.Begin();
+            testObject.Update(gameTime);
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
---
+++ MosquitoTime/GameObject.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosquitoTime
+{
+    public class GameObject
+    {
+
+        Sprite _sprite;
+        Transform _transform;
+
+        public GameObject (Sprite sprite, Transform transform)
+        {
+            _sprite = sprite;
+            _transform = transform;
+        }
+
+        public bool OnCollide(Rectangle otherBounds)
+        {
+            return _sprite.Bounds.Intersects(otherBounds);
+        }
+
+        public void Move(Vector2 offset)
+        {
+            _transform.TranslatePosition(offset);
+            _sprite.UpdateBounds(_transform);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _transform.CheckBounds(_sprite);
+            Move();
+        }
+        public void Draw()
+        {
+            _sprite.Draw();
+        }
+    }
+
+
+
+
+    public struct Transform
+    {
+        public void TranslatePosition(Vector2 offset)
+        {
+            //Position = new Vector2(Position.X + offset.X, Position.Y + offset.Y);
+            Position += offset;
+        }
+
+        public bool CheckBounds(Sprite sprite)
+        {
+
+            return false;
+        }
+
+        public Transform(Vector2 position, Vector2 direction, float rotation, float scale)
+        {
+            this.Position = position;
+            this.Direction = direction;
+            this.Rotation = rotation;
+            this.Scale = scale;
+        }
+
+        public Vector2 Position;
+        public Vector2 Direction;
+        public float Rotation;
+        public float Scale;
+    }
+
+
+    public struct Sprite
+    {
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(SpriteSheet, Bounds, );
+
+        }
+
+        public Sprite(Texture2D texture, Rectangle bounds, float scale)
+        {
+            this.SpriteSheet = texture;
+            this.Bounds = bounds;
+            this.Scale = scale;
+        }
+
+        public Texture2D SpriteSheet;
+        public Rectangle Bounds;
+        public float Scale;
+
+        public void UpdateBounds(Transform transform)
+        {
+            Bounds = new Rectangle();
+        }
+    }
+}
